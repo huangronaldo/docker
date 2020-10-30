489395a1e06f164817982786394d25344864ef1b
huangronaldo <huangronaldo@qq.com>
2020-10-30 16:39:34 +0800

    Update entrypoint.sh

--- keepalived+nginx/entrypoint.sh
+++ keepalived+nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console &
-
 nginx -g "daemon off;"
+#echo `ps -ef` > /tmp/keepalived.txt
+#rm -rf /var/run/keepalived/keepalived.pid
+/usr/sbin/keepalived -n -l -D -f /etc/keepalived/keepalived.conf --dont-fork --log-console &
